eefed48d335fdb48f56f5b322c04d5c8d8cc7181
Rinie Kervel <rinie.kervel@centric.eu>
2024-02-02 07:27:03 +0100

    receiving  [LilyGo_SSD1306,WebUI,RF,RF2,Pilight,rtl_433]

--- src/signalDecoder.cpp
+++ src/signalDecoder.cpp
@@ -479,8 +479,10 @@ void rtl_433_DecoderTask(void* pvParameters) {
     int events = 0;
 #ifdef RADIOLIBSX127X
     if (rtl_433_ESP::_decodePulseGapDurationCallback) {
+		int ip = 0;
+		int ig = 0;
 	    for (int i = 0; i < rtl_pulses->num_pulses; i++) {
-			int ip = rtl_433_ESP::_decodePulseGapDurationCallback(rtl_pulses->pulse[i]);
+			ip = rtl_433_ESP::_decodePulseGapDurationCallback(rtl_pulses->pulse[i]);
 			if (ip < 0) {
 				events = 0;
 			}
@@ -496,6 +498,9 @@ void rtl_433_DecoderTask(void* pvParameters) {
 			}
 			events = 1;
 		}
+	  if ((ip == 0) && (ig == 0)) { // signal end with 0 unless decoder ended loop
+	  	ip = rtl_433_ESP::_decodePulseGapDurationCallback(0);
+	  }
 	}
 	if (events == 0)
 #endif
